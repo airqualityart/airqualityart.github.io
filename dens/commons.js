836618c35e83eeca93e25fa4caff3932a9235a6f
Air Quality And Related Topics <45898680+airqualityart@users.noreply.github.com>
2023-12-10 10:35:14 +0100

    Implemented the differential equation order picker.

--- dens/commons.js
+++ dens/commons.js
@@ -13,28 +13,52 @@ function Tagify(s, tag, attributes) {
     return out + ">" + s + "</" + tag + ">";
 }
 
-function LetterPicker(caller) {
-    // Create and show the letter-picking window.
-    const current = document.getElementById(caller.id).innerHTML;
-    let letters = "abcdefghijklmnopqrstuvwxyz";
+function CharacterPicker(characters, caller, callback, current) {
+    // Create and show a character-picking window.
     let content = "";
-    for (let i = 0; i < 2; i++) {
-        if (i == 1) letters = letters.toUpperCase();
-        var s = ""
-        for (var j = 0; j < letters.length; j++) {
-            var attributes = {onclick: "LetterPickerSelected('" + letters[j] + "', '" + caller.id + "')"};
-            if (letters[j] == current) attributes["class"] = "active";
-            s += Tagify(letters[j], "button", attributes);
+    for (let i = 0; i < characters.length; i++) {
+        var s = "";
+        for (var j = 0; j < characters[i].length; j++) {
+            var c = characters[i][j];
+            var attributes = {onclick: callback + "('" + c + "', '" + caller.id + "')"};
+            if (c == current) attributes["class"] = "active";
+            s += Tagify(c, "button", attributes);
         }
-        content += Tagify(s, "div")
+        content += Tagify(s, "div");
     }
-    let picker = document.getElementById("letterpicker");
-    picker.innerHTML = content;
-    picker.style.display = "flex";
+    document.getElementById("shadedbackground").style.display = "block";
+    document.getElementById("characterpicker").innerHTML = content;
+    document.getElementById("characterpicker").style.display = "flex";
 }
 
-function LetterPickerSelected(letter, id) {
+function LetterPicker(caller) {
+    // Create and show the letter-picking window.
+    const letters = "abcdefghijklmnopqrstuvwxyz";
+    const current = document.getElementById(caller.id).innerHTML;
+    CharacterPicker([letters, letters.toUpperCase()], caller, "OnLetterPickerSelected", current);
+}
+
+function OrderPicker(caller) {
+    // Create and show the order-picking window.
+    const current = document.getElementById(caller.id).innerHTML[0];
+    CharacterPicker(["123456789"], caller, "OnOrderPickerSelected", current);
+}
+
+function OnLetterPickerSelected(letter, id) {
     // Callback for when a letter button is clicked in the letter picker.
     document.getElementById(id).innerHTML = letter;
-    document.getElementById("letterpicker").style.display = "none";
+    document.getElementById("characterpicker").style.display = "none";
+    document.getElementById("shadedbackground").style.display = "none";
+}
+
+function OnOrderPickerSelected(order, id) {
+    // Callback for when a number button is clicked in the order picker.
+    let s = "th";
+    if (order == "1")
+        s = "st";
+    else if (order == "2")
+        s = "nd";
+    document.getElementById(id).innerHTML = order + Tagify(s, "sup") + " order";
+    document.getElementById("characterpicker").style.display = "none";
+    document.getElementById("shadedbackground").style.display = "none";
 }
